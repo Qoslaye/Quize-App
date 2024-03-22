79221bf61e04b6da84420f7d130645906da83537
Qoslaye <iamqoslaye@gmail.com>
2024-03-22 13:41:37 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Quize } from './Components/Quize/Quize'
+
+const App = () => {
+  return (
+    <div> 
+      <Quize />
+    </div>
+  )
+}
+
+export default App
